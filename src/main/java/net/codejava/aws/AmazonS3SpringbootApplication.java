ba7c44662c9782efcb20ac5bb99ea48a1aa3531c
nitinsharma <nitinsharma1108@gmail.com>
2023-06-08 21:54:38 +0530

    Code to connect S3 bucket without credential and upload the document to the s3 bucket

---
+++ src/main/java/net/codejava/aws/AmazonS3SpringbootApplication.java
@@ -0,0 +1,13 @@
+package net.codejava.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonS3SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonS3SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/net/codejava/aws/ServletInitializer.java
@@ -0,0 +1,13 @@
+package net.codejava.aws;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AmazonS3SpringbootApplication.class);
+	}
+
+}
